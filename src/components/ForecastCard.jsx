9cf18d4cb15d65784bd3296ece750d02c2ddc319
Ugochukwu Uzoh <ugochukwuuzoh@gmail.com>
2024-01-06 19:12:25 +0200

    change text color to black

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -5,7 +5,7 @@ const ForecastCard = ({ aqi, date }) => {
   return (
     <div className="w-screen md:w-full flex items-center justify-center">
       <div className="bg-white mx-3 rounded-full md:h-36 w-[80%] md:w-20 p-2 flex md:flex-col items-center justify-between mb-2 md:mb-0">
-        <div className={`bg-${color} w-10 h-10 md:w-14 md:h-14 text-lg rounded-full flex flex-col items-center justify-center md:mb-8 order-last md:order-first`}>
+        <div className={`bg-${color} text-[#333] w-10 h-10 md:w-14 md:h-14 text-lg rounded-full flex flex-col items-center justify-center md:mb-8 order-last md:order-first`}>
           <p>{aqi}</p>
         </div>
         <p className="text-[#333] font-semibold md:font-normal md:text-[12.5px] md:mb-2">{date}</p>
