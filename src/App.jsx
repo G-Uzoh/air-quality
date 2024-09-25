c4114fe0f56787313a949a0be6fd2de46c709769
George Uzoh <ugochukwuuzoh@gmail.com>
2024-09-25 12:42:43 +0300

    implement logic to fetch dropdown suggestions for locations

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,101 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import Header from "./components/Header";
 import axios from "axios";
 import AirQuality from "./pages/AirQuality";
 import Swal from "sweetalert2";
 import { getDateString } from "./helperFunctions/getDateString";
+import { useDebounce } from "./customHooks/useDebounce";
 
 function App() {
   const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [coordinates, setCoordinates] = useState(null);
+  const [coordinates, setCoordinates] = useState({ lat: 0, lon: 0 });
   const [currentAirPollution, setCurrentAirPollution] = useState(null);
   const [airPollutionForecast, setAirPollutionForecast] = useState(null);
   const [date, setDate] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [displayText, setDisplayText] = useState(true);
+  const [geocodeFeatures, setGeocodeFeatures] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
+  const [dropdownSuggestions, setDropdownSuggestions] = useState([]);
+
+  // Dropdown container reference
+  const dropdownRef = useRef(null);
 
   // Get date in new format dd MM DD
   const dateOfCurrentPollutionData = getDateString(date);
 
+  // Debounce city value with 1s delay to avoid unnecessary calls to API
+  const debouncedCity = useDebounce(city, 1000);
+
   // API URLs
-  const geocodingApiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${import.meta.env.VITE_GEOCODING_API_TOKEN}`;
+  const geocodingApiUrl = `https://api.mapbox.com/search/geocode/v6/forward?q=${debouncedCity}&access_token=${import.meta.env.VITE_GEOCODING_API_TOKEN}`;
 
   const airPollutionApiUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${coordinates?.lat}&lon=${coordinates?.lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`;
 
   const airPollutionForecastApiUrl = `https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${coordinates?.lat}&lon=${coordinates?.lon}&appid=${import.meta.env.VITE_WEATHER_API_KEY}`;
+
+  // Memoized function to fetch location suggestions
+  const fetchLocationSuggestions = useCallback(async () => {
+    try {
+      const res = await axios.get(geocodingApiUrl);
+      const { features } = res.data;
+
+      // Save the full features array and display the first 5 suggestions
+      setGeocodeFeatures(features);
+      const suggestions = features.slice(0, 5).map((feature) => {
+        const { full_address } = feature.properties;
+        const fullAddressParts = full_address.split(', ');
+
+        if (fullAddressParts.length > 3) {
+          return `${fullAddressParts[0]}, ${fullAddressParts[1]}, ${fullAddressParts[fullAddressParts.length - 1]}`;
+        }
+
+        return full_address;
+      });
+
+      setDropdownSuggestions(suggestions);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, [geocodingApiUrl]);
+
+  // Fetch location suggestions when the city value changes
+  useEffect(() => {
+    if (!isSelected) {
+      fetchLocationSuggestions();
+    }
+  }, [isSelected, fetchLocationSuggestions]);
+
+  // Handle suggestion selection from dropdown
+  const handleSelectSuggestion = (i) => {
+    const selectedFeature = geocodeFeatures[i];
+    const selectedAddress = selectedFeature.properties.full_address;
+    setCity(selectedAddress);
+    setDropdownSuggestions([]); // Clear suggestions after selecting one
+    setIsSelected(true);
+  }
+
+  // Close the dropdown when clicking outside of it or pressing the escape key
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setDropdownSuggestions([]); // Close the dropdown
+      }
+    }
+
+    const handleEscapeKeyPress = (e) => {
+      if (e.key === 'Escape') setDropdownSuggestions([]); // Close the dropdown
+      if (e.key === 'Enter') setDropdownSuggestions([]); // Close the dropdown
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('keydown', handleEscapeKeyPress);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscapeKeyPress);
+    }
+  }, []);
 
   const handleChange = (e) => {
     const searchValue = e.target.value.toLowerCase();
@@ -49,14 +121,13 @@ function App() {
   const getCoordinates = async () => {
     try {
       const res = await axios.get(geocodingApiUrl);
-      const data = res.data;
+      const { features } = res.data;
       
-      if (data.features.length > 0) {
-        const [lon, lat] = data.features[0].geometry.coordinates;
-        const location = data.features[0].place_name.split(', ').slice(-1).join('');
+      if (features.length > 0) {
+        const [lon, lat] = features[0].geometry.coordinates;
+        const locationCoordinates = { lat, lon };
         
-        setCoordinates({ lat, lon });
-        setCountry(location);
+        setCoordinates(locationCoordinates);
 
       } else {
         Swal.fire({
@@ -77,6 +148,8 @@ function App() {
   // Air pollution current data
   useEffect(() => {
     const fetchData = async () => {
+      if (coordinates?.lat === 0 && coordinates?.lon === 0) return;
+
       try {
         setIsLoading(true);
         setDisplayText(false);
@@ -111,6 +184,8 @@ function App() {
 
   // Air pollution forecast data
   useEffect(() => {
+    if (coordinates?.lat === 0 && coordinates?.lon === 0) return;
+
     const fetchForecastData = async () => {
       try {
         setIsLoading(true);
@@ -164,7 +239,6 @@ function App() {
       <AirQuality
         {...currentAirPollution}
         city={city}
-        country={country}
         handleSearch={handleSearch}
         handleChange={handleChange}
         handleKeyPress={handleKeyPress}
@@ -173,6 +247,9 @@ function App() {
         airPollutionForecast={airPollutionForecast}
         isLoading={isLoading}
         displayText={displayText}
+        dropdownSuggestions={dropdownSuggestions}
+        handleSelectSuggestion={handleSelectSuggestion}
+        dropdownRef={dropdownRef}
       />
     </div>
   );
